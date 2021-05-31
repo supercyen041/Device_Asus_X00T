7f55e74f9bb701f1d61c90169fd504cdc45e9dbf
supercyen041 <supercyen041@gmail.com>
2021-05-31 22:22:49 +0530

    pixel prop

--- init/init_X00T.cpp
+++ init/init_X00T.cpp
@@ -186,9 +186,9 @@ void vendor_check_variant()
 
     // Safetynet Workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    property_override("ro.build.description", "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-    property_override("ro.system.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210505.003 7255357 release-keys");
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys");
+    property_override("ro.system.build.fingerprint", "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys");
 
 }
 
